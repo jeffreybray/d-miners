cf4d1331d83bdb327f2ebb50f2a12cf6f3e10d68
ncgan <ganeshn.8844@gmail.com>
2023-09-22 10:30:54 -0500

    Added Report Section

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ reddit_df = pd.read_csv('data/redditdata.csv')
 spvreportdata.fillna(0,inplace=True)
 spvreportdata.columns = ['Services','Type','Feb','Mar','Apr','May','Jun','Total']
 
+casedata_Provider = pd.melt(spvreportdata, id_vars=['Type','Services'],value_vars=['Feb','Mar','Apr','May','Jun'],
 
+        var_name='Month', value_name='Value')
 
 # Define the labels and values hierarchically
 labels = ["Identify", "Collaborate & Track", "Report"]
@@ -31,7 +33,8 @@ parents = ["", "", ""]
 source_filter_fig = go.Figure(go.Treemap(
     labels=labels,
     parents=parents,
-    marker_colors=color_palette_discrete
+    marker_colors=color_palette_discrete,
+    insidetextfont=dict(size=34)
 ))
 
 # Customize the layout
@@ -40,6 +43,61 @@ source_filter_fig.update_layout(
     margin=dict(t=0, l=0, r=0, b=0)  # Adjust margins as needed
 )
 source_filter_fig.update_layout(title="Source Filter",height=100)
+
+
+
+labels = ['19-25','25-59','60+']
+values = [4,32,9]
+
+age_fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3,textinfo='label+percent',marker_colors= ["#FFD166","#6A0572","#DA9B47","#00847A"])])
+age_fig.update_layout(legend=dict(orientation="h", y=-0.2),title={"text": "Age-Ranges BreakDown",
+                             "y":0.95,
+                             "x":0.5,
+                             "xanchor": "center",
+                             "yanchor": "top"})
+
+
+labels = ['Black/African American','American Indian/ Native Alaskan','Hispanic','White/Caucasian','Other/Mixed Race/Ethnicity']
+values = [43, 1, 10,44,2]
+
+racial_fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent',hole=.3,marker_colors= ["#FFD166","#6A0572","#DA9B47","#00847A"])])
+racial_fig.update_layout(legend=dict(orientation="h", y=-0.2),title={"text": "Racial/Ethnic Makeup BreakDown",
+                             "y":0.95,
+                             "x":0.5,
+                             "xanchor": "center",
+                             "yanchor": "top"})
+
+
+
+labels = ['Female','Male','Non-binary']
+values = [60, 38, 2]
+
+gender_fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3,textinfo='label+percent',marker_colors= ["#FFD166","#6A0572","#DA9B47","#00847A"])])
+gender_fig.update_layout(legend=dict(orientation="h", y=-0.2),title={"text": "Racial/Ethnic Makeup BreakDown",
+                             "y":0.95,
+                             "x":0.5,
+                             "xanchor": "center",
+                             "yanchor": "top"})
+
+casedata_Provider = pd.melt(spvreportdata, id_vars=['Type','Services'],value_vars=['Feb','Mar','Apr','May','Jun'],
+
+        var_name='Month', value_name='Value')
+
+
+news_count = get_top5_agg_df(casedata_Provider,['Type'])
+spvreport_fig = px.pie(news_count,"Type","count",color_discrete_sequence =  ["#FFD166","#6A0572","#DA9B47","#00847A"])
+spvreport_fig.update_layout(legend=dict(orientation="h", y=-0.2),title={
+                             "y":0.95,
+                             "x":0.5,
+                             "xanchor": "center",
+                             "yanchor": "top"})
+spvreport_fig.update_layout(height = 700)
+
+
+type = "Medical providers"
+dff = casedata_Provider[(casedata_Provider["Type"] == type)]
+
+bar_fig = bar_chart(dff,"Month", "Value","Month","Value", color="Services")
 
 def show_news():
     #top_headlines_title = [str(translate(model,tok,top_headlines['articles'][i]['title'])[0]) for i in range(len(top_headlines['articles']))]
@@ -78,6 +136,25 @@ html.Br(),
 
 ])
 
+
+report_layout = html.Div(children=[
+                        dbc.Row(
+                        [
+                            dbc.Col(dcc.Graph(id="gender_fig",figure=gender_fig),width=3),
+                            dbc.Col(dcc.Graph(id="racial_fig",figure=racial_fig),width=5),
+                            dbc.Col(dcc.Graph(id="age_fig",figure=age_fig),width=3),
+                        ],
+                    ),
+
+                   dbc.Row(
+                        [
+                            dbc.Col(dcc.Graph(id="spvreport_fig",figure=spvreport_fig),width=4),
+                            dbc.Col(dcc.Graph(id="bar_fig",figure=bar_fig),width=7)
+                        ],
+                    )
+])
+
+
 app.layout = html.Div(
         [
         dbc.Col(
@@ -89,8 +166,6 @@ app.layout = html.Div(
                 ], justify="around"),
        html.Br(),html.Div(id='tabs-content-example-graph')
 ])
-
-
 
 
 
@@ -106,6 +181,8 @@ def render_content(source_filter_clickData):
     if label == 'Identify':
         return identify_layout
 
+    elif label == 'Report':
+        return report_layout
 
 
 if __name__ == "__main__":
